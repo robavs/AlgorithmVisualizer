b0dc4c6f78ef049161a4b80fe969c6aa1fd8a535
Aleksa Robavs <robavs.aleksa@gmail.com>
2023-11-07 23:23:52 +0100

    added animation for showing shortestPath

--- src/controller/algorithms/bfs.ts
+++ src/controller/algorithms/bfs.ts
@@ -1,18 +1,27 @@
 import { Coords, GridCell, ItemType } from "../../model";
 import { coords, isValid } from "./utils";
 import { matrixCell } from "../../view/gridTable";
-import { BehaviorSubject, Observable, Subscription, count, distinctUntilChanged, elementAt, filter, first, from, interval, last, map, of, scan, skip, switchMap, take, takeLast, takeUntil, tap } from "rxjs";
+import { BehaviorSubject, Observable, Subscription, combineLatest, concatMap, count, delay, distinctUntilChanged, elementAt, filter, first, from, interval, last, map, mergeMap, of, scan, skip, switchMap, take, takeLast, takeUntil, tap, throttleTime } from "rxjs";
 
 // mora vidim kako da ne porsledjujem kao argument vec da ih prensem preko import/export
 export const bfs = (grid: GridCell[][]): void => {
     let queue$: Observable<Coords> = of([])
     const isCheeseFound$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false)
     const queueSize$: BehaviorSubject<number> = new BehaviorSubject<number>(1)
+    const currentPath$: BehaviorSubject<number[][]> = new BehaviorSubject<number[][]>([])
+    let shortestPath$: Observable<number[]> = of()
+    const gridPath: number[][][][] = []
 
-    for (let i = 0; i < grid.length; i++)
-        for (let j = 0; j < grid[i].length; j++)
-            if (grid[i][j] === ItemType.Mouse)
+    for (let i = 0; i < grid.length; i++) {
+        gridPath[i] = []
+        for (let j = 0; j < grid[i].length; j++) {
+            gridPath[i][j] = [[i, j]]
+            if (grid[i][j] === ItemType.Mouse) {
                 queue$ = of([i, j])
+                grid[i][j] = ItemType.Wall
+            }
+        }
+    }
 
     const intervalSubscription$: Subscription = interval(250)
         .pipe(
@@ -26,8 +35,44 @@ export const bfs = (grid: GridCell[][]): void => {
     // treba da se proveri ako nije uopste nadjen da se i tada ukine subsrkipcija
     isCheeseFound$.subscribe({
         next: (isFound: boolean) => {
-            if (isFound)
+            if (isFound) {
                 intervalSubscription$.unsubscribe()
+
+                const sub = new BehaviorSubject(shortestPath$)
+
+                // interval(500).pipe(
+                //     mergeMap((val) => {
+                //         return of(shortestPath$)
+                //     }),
+                //     tap((val) => {
+                //         val.pipe(take(1)).subscribe(console.log)
+                //     })
+                // )
+                //     .subscribe()
+                shortestPath$
+                    .pipe(
+                        mergeMap((coords, index) => {
+                            return of(coords).pipe(
+                                delay(50 * (index + 1))
+                            )
+                        }),
+                        tap(([x, y]) => {
+                            matrixCell[x][y].classList.add("shortest-path")
+                        })
+                    )
+                    .subscribe()
+
+                // interval(500).pipe(
+                //     take(1),
+                //     mergeMap(() => shortestPath$),
+                //     switchMap(([x, y]) => {
+                //         matrixCell[x][y].classList.add("shortest-path")
+                //         return of([x, y])
+                //     })
+                // )
+                //     .subscribe()
+
+            }
         }
     })
 
@@ -40,6 +85,8 @@ export const bfs = (grid: GridCell[][]): void => {
             .pipe(
                 take(queueSize$.value),
                 switchMap(([x, y]: Coords) => {
+                    currentPath$.next(gridPath[x][y])
+
                     const nextSquares: Coords[] = []
                     for (const [dx, dy] of coords) {
                         const newX: number = x + dx
@@ -51,11 +98,15 @@ export const bfs = (grid: GridCell[][]): void => {
                 // mozda da vidim da se ne prosleduje grid nego da ga negde stavim u neki type
                 filter(coords => isValid(coords, grid)),
                 tap(([x, y]: Coords) => {
+                    if (isCheeseFound$.value) return
+                    gridPath[x][y] = [...currentPath$.value, [x, y]]
                     if (grid[x][y] === ItemType.Cheese) {
                         matrixCell[x][y].classList.add("found")
+                        shortestPath$ = from(gridPath[x][y])
                         isCheeseFound$.next(true)
+                        console.log("PATH JE", gridPath[x][y])
                     }
-                    if (!grid[x][y]) {
+                    else {
                         matrixCell[x][y].classList.add("visited")
                         grid[x][y] = ItemType.Wall
                         newCoords.push([x, y])
