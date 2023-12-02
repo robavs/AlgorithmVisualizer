effe687fe2b21abc37ae00e6840b9d813d988b56
Aleksa Robavs <robavs.aleksa@gmail.com>
2023-12-02 16:18:24 +0100

    samo jedan sirac i mis

--- src/controller/baseFunctions.ts
+++ src/controller/baseFunctions.ts
@@ -7,10 +7,29 @@ export const gridTableFunctions = (): void => {
     const icons = [...document.querySelectorAll(".icon")] as HTMLImageElement[]
     const startBtn = document.querySelector(".start-btn") as HTMLDivElement
 
+    const isMousePlaced$ = new BehaviorSubject<boolean>(false)
+    const isCheesePlaced$ = new BehaviorSubject<boolean>(false)
+
     // razmisli da l mozda da stavim da mi tableCells bude BehaviourSubject
     const isPlacingAllowed: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true)
     const tableCells = [...document.querySelectorAll(".cell")] as HTMLTableCellElement[]
     const grid: GridCell[][] = Array(15).fill(0).map(() => Array(40).fill(null))
+
+    isCheesePlaced$.subscribe({
+        next: (isPlaced: boolean) => {
+            const cheese = icons.filter(img => img.alt === "Cheese")[0]
+            cheese.style.opacity = isPlaced ? "0.5" : "1"
+            cheese.style.pointerEvents = isPlaced ? "none" : "auto"
+        }
+    })
+
+    isMousePlaced$.subscribe({
+        next: (isPlaced: boolean) => {
+            const mouse = icons.filter(img => img.alt === "Mouse")[0]
+            mouse.style.opacity = isPlaced ? "0.5" : "1"
+            mouse.style.pointerEvents = isPlaced ? "none" : "auto"
+        }
+    })
 
     // onemogucava da imas drag event za elemente koji se vec nalaze na tabeli
     fromEvent(tableCells, "dragstart")
@@ -27,6 +46,11 @@ export const gridTableFunctions = (): void => {
             event.preventDefault()
             const cell = event.currentTarget as HTMLTableCellElement
             if (!cell.childNodes.length) return
+
+            if ((cell.childNodes[0] as HTMLImageElement).alt === "Cheese")
+                isCheesePlaced$.next(false)
+            else if ((cell.childNodes[0] as HTMLImageElement).alt === "Mouse")
+                isMousePlaced$.next(false)
 
             // brisanje
             grid[Number(cell.getAttribute("x"))][Number(cell.getAttribute("y"))] = null
@@ -64,19 +88,27 @@ export const gridTableFunctions = (): void => {
                 const x: number = Number(cell.getAttribute("x"))
                 const y: number = Number(cell.getAttribute("y"))
 
+                if (selectedIcon.alt === "Cheese" && isCheesePlaced$.value ||
+                    selectedIcon.alt === "Mouse" && isMousePlaced$.value
+                )
+                    return
+
                 // mozda i ovo moze drugacije nekako
                 if (selectedIcon.alt === "Cheese") {
                     grid[x][y] = ItemType.Cheese
+                    isCheesePlaced$.next(true)
                 }
                 else if (selectedIcon.alt === "Wall") {
                     grid[x][y] = ItemType.Wall
                 }
                 else {
                     grid[x][y] = ItemType.Mouse
+                    isMousePlaced$.next(true)
                 }
 
                 const imgIcon = document.createElement("img")
                 imgIcon.classList.add("icon")
+                imgIcon.alt = selectedIcon.alt
                 imgIcon.src = selectedIcon.src
                 cell.appendChild(imgIcon)
                 isPlacingAllowed.next(false)
